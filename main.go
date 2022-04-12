9e2e64112aaa01070a0b6ab4e4df061c7d0ee0b5
村上恭哉 <mkkyyyy@isa3-dhcp-190-228.naist.jp>
2022-04-12 16:11:15 +0900

    file create open

--- main.go
+++ main.go
@@ -1,23 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func do(i interface{}) {
-	switch variable := i.(type) {
-	case int:
-		fmt.Println(variable)
-	case string:
-		fmt.Println(variable)
-	case func():
-		variable()
-	default:
-		fmt.Println("Default")
-	}
-}
+type aaa string
 
 func main() {
+	f, err := os.Open("text.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	do(22)
-	do("stst")
-	do(func() { fmt.Println("aaa") })
+	data := make([]byte, 1024)
+	count, err := f.Read(data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(count)
+	fmt.Println(data, string(data[:count]))
+
+	fmt.Println()
+	fmt.Println("Create method")
+	fmt.Println()
+
+	f, err = os.Create("write.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	str := "Write this file by Golang!"
+	data = []byte(str)
+	count, err = f.Write(data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(count)
 }
