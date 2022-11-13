3358d6c8200da4842257a53bd821ee2cfe3f29b0
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-11-13 20:42:13 +0900

    fix pkgbufio.go

--- file/pkgbufio.go
+++ file/pkgbufio.go
@@ -21,3 +21,18 @@ func ReadFileWithBuf() {
 	fmt.Println(s.Text())
 
 }
+
+func ReadWord() {
+	f, err := os.Open("file/text.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanWords)
+
+	for s.Scan() {
+		fmt.Println(s.Text())
+	}
+}
